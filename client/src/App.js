546636c01abc5a1b02ac22b72a7194a2ba3cbd9b
A-Hussamadin <h.abushelha@gmail.com>
2018-03-31 00:02:56 +0300

    adding react router4

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './components/layouts/Home';
+import About from './components/layouts/About';
+import Layout from './components/layouts/Layout';
+import NewsItemDetail from './components/presentation/NewsItemDetail';
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Layout>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path='/news/:id' component={NewsItemDetail} />
+        </Layout>
+      </BrowserRouter>
     );
   }
 }
